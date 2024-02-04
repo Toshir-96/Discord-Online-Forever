95b2088773dcd8fdd0fa55862384c6d46e3be0d3
тσѕнıяσ <99965804+Toshir-96@users.noreply.github.com>
2024-02-04 20:31:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ const keep_alive = require('./keep_alive.js')
 const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+  console.error(err);
+  setCustomPresence();
 });
+
+bot.on("ready", () => {
+  console.log(`Logged in as ${bot.user.username}`);
+  setCustomPresence();
+});
+
+function setCustomPresence() {
+  bot.editStatus({
+    name: 'With ur Mom',
+    type: 0, // 0 for "Playing", adjust as needed
+    status: 'dnd',
+  });
+}
 
 bot.connect(); // Get the bot to connect to Discord
